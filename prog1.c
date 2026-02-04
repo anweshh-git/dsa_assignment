978e7f197eb4c9d9560e6c28ac50e03730991ec8
anweshh-git <anweshhumagain107@gmail.com>
2026-02-04 22:09:21 +0545

    checking for balanced no of brackets in expression

--- prog1.c
+++ prog1.c
@@ -17,7 +17,7 @@ bool isempty(stack *s) {
     return s->top == -1;
 }
 //push operation
-bool push(Stack *s, char c) {
+bool push(stack *s, char c) {
     if (s->top >= MAX_SIZE - 1) return false; // Stack overflow
     s->items[++(s->top)] = c;
     return true;
@@ -37,4 +37,31 @@ char peek(stack *s) {
         exit(EXIT_FAILURE);
     }
     return s->items[s->top];
+}
+//checking if expression has balanced number of brackets
+bool isblanced(const char *e) { // e for expression
+    char stack[100];
+    int top = -1;
+    
+    for (int i = 0; e[i]; i++) {
+        char c = e[i];
+        
+        //push opening brackets inside stacl
+        if (c == '(' || c == '[' || c == '{') {
+            stack[++top] = c;
+        }
+        //checing closing brackets
+        else if (c == ')' || c == ']' || c == '}') {
+            if (top == -1) return false; 
+            
+            char open = stack[top--];
+            if ((c == ')' && open != '(') ||
+                (c == ']' && open != '[') ||
+                (c == '}' && open != '{')) {
+                return false;
+            }
+        }
+    }
+    
+    return top == -1; //is true if stack is empty
 }
