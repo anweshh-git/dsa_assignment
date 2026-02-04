071aefa9c3386b9d51a6523c2d3f1fd7dbeedd46
anweshh-git <anweshhumagain107@gmail.com>
2026-02-04 22:02:47 +0545

    push,pop and peek operation done

--- prog1.c
+++ prog1.c
@@ -16,3 +16,25 @@ void initializationstack(stack *s) {
 bool isempty(stack *s) {
     return s->top == -1;
 }
+//push operation
+bool push(Stack *s, char c) {
+    if (s->top >= MAX_SIZE - 1) return false; // Stack overflow
+    s->items[++(s->top)] = c;
+    return true;
+}
+//pop operation
+char pop(stack *s) {
+    if (isempty(s)) {
+        printf("Stack underflow\n");
+        exit(EXIT_FAILURE);
+    }
+    return s->items[(s->top)--];
+}
+//peek operation
+char peek(stack *s) {
+    if (isempty(s)) {
+        printf("Stack is empty\n");
+        exit(EXIT_FAILURE);
+    }
+    return s->items[s->top];
+}
