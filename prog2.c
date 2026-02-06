0ff642277b6fb5059fa99841c69ed5d5aa6877ed
anweshh-git <anweshhumagain107@gmail.com>
2026-02-06 18:34:21 +0545

    function for infix to postfix

--- prog2.c
+++ prog2.c
@@ -27,3 +27,27 @@ int prec(char c) {
     if (c == '*' || c == '/') return 2;
     return 0;
 }
+//function to convert infix to postfix
+void infixToPostfix(char *infix, char *postfix) {
+    int j = 0;
+    for (int i = 0; infix[i]; i++) {
+        char c = infix[i];
+        
+        if (isdigit(c)) postfix[j++] = c;         
+        else if (c == '(') pushOp(c);            
+        else if (c == ')') {                     
+            while (!isEmptyOp() && peekOp() != '(')
+                postfix[j++] = popOp();
+            popOp(); // Remove '('
+        }
+        else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            while (!isEmptyOp() && peekOp() != '(' && 
+                   prec(peekOp()) >= prec(c))
+                postfix[j++] = popOp();
+            pushOp(c);
+        }
+    }
+    while (!isEmptyOp()) postfix[j++] = popOp();   //pop remaining operators
+    postfix[j] = '\0';
+}
+
